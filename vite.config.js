f7e03f194f7f95c992e01bfafc60765392593f91
anasayago <anasayago@gmail.com>
2025-10-02 18:08:24 +0200

    Prueba de configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /** @type {import('vite').UserConfig} */
 export default {
     root: '.',
-    base: '/LaCalledelasMujeresAndalucia/',
+    base: './',
     build: {
         outDir: 'dist',
         emptyOutDir: true
