b866d61284e26fbf7f4af86e5b61cb23e5df35f4
anasayago <anasayago@gmail.com>
2025-10-02 17:53:01 +0200

    cambio en base de vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /** @type {import('vite').UserConfig} */
 export default {
     root: '.',
-    base: './',
+    base: '/LaCalledelasMujeresAndalucia/',
     build: {
         outDir: 'dist',
         emptyOutDir: true
