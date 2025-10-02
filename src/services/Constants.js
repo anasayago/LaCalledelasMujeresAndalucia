5bd8dce85e8af506db772a4930717157edb644e3
anasayago <anasayago@gmail.com>
2025-10-02 18:32:00 +0200

    Cambios de despliegue a GitHub Pages

--- src/services/Constants.js
+++ src/services/Constants.js
@@ -10,6 +10,7 @@ export const maxRadius = 18;
 export const FEMALE = 'Female';
 export const MALE = 'Male';
 //export const URL_DATA = 'https://raw.githubusercontent.com/geochicasosm/lascallesdelasmujeres/master';
+
 export const URL_DATA = '/LaCalledelasMujeresAndalucia/';
 export const SOURCE_TYPES_LIST = ['line', 'fill'];
 
